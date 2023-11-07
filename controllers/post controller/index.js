601aa8f6400e79f6eec349c4a91df01837836aba
Brayan M <mackony1000@gmail.com>
2023-11-07 18:09:43 -0500

    [add] creando un archivo para exportar por defecto

---
+++ controllers/post controller/index.js
@@ -0,0 +1,8 @@
+
+module.exports= {
+    createPost:require("./createPost.js"),
+    getAllPosts: require("./getALLPosts.js"),
+    getPostById: require("./getPosById.js"),
+    updatePost: require("./updatePosts.js"),
+    deletePost: require("./deletePosts.js")
+};
